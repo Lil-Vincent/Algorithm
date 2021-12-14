339191e7023fcc42ec862e7de8355b728c6d423d
Lil <1378951829@qq.com>
2021-12-14 23:01:53 +0800

    归并排序

--- src/main/java/com/lil/算法基础/_1_基础算法/B_归并排序.java
+++ src/main/java/com/lil/算法基础/_1_基础算法/B_归并排序.java
@@ -1,14 +1,10 @@
 package com.lil.算法基础._1_基础算法;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-
 /**
  * @author Lil
- * @date 2021/12/08 12:38
- **/
+ * @date 2021/11/27 11:36
+ */
 public class B_归并排序 {
     public static void main(String[] args) {
         Scanner in =  new Scanner(System.in);
@@ -17,28 +13,29 @@ public class B_归并排序 {
         for (int i = 0; i < n; i++) {
             q[i] = in.nextInt();
         }
-
         mergeSort(q, 0, n - 1);
-        for (int i = 0; i < n; i++) System.out.print(q[i] + " ");
+        for (int i = 0; i < n; i++)
+            System.out.print(q[i] + " ");
     }
-    public static void mergeSort(int q[], int l, int r){
-        if (l >= r)return;
 
-        int mid = l + r >> 1;
-        mergeSort(q, l, mid);
-        mergeSort(q, mid + 1, r);
+    private static void mergeSort(int[] q, int l, int r) {
 
-        int i = l, j = mid + 1, k = 0;
-        int temp[] = new int[r - l + 1];
-        while (i <= mid && j <= r){
-            if (q[i] < q[j])
-                temp[k++] = q[i++];
-            else
-                temp[k++] = q[j++];
+        if (l >= r) return;
+        int mid = l + r >> 1, i = l, j = mid + 1;
+        mergeSort(q, i, mid);
+        mergeSort(q, j, r);
+
+        int[] temp = new int[r - l + 1];
+        int k = 0;
+        while (i <= mid && j <= r) {
+            if (q[i] < q[j]) temp[k ++] = q[i ++];
+            else temp[k ++] = q[j ++];
         }
         while (i <= mid) temp[k ++] = q[i ++];
-        while (j <= r)   temp[k ++] = q[j ++];
+        while (k <= r) temp[k ++] = q[j ++];
 
         for (i = l, j = 0; i <= r; i++, j++) q[i] = temp[j];
     }
+
+
 }
