428338b321254d9022896ae27fddc117b07db9c0
Lil <1378951829@qq.com>
2022-03-18 13:02:25 +0800

    update .gitignore

--- src/main/java/com/lil/leetcode/LeetCode_1_两数之和.java
+++ src/main/java/com/lil/leetcode/LeetCode_1_两数之和.java
@@ -1,5 +1,7 @@
 package com.lil.leetcode;
 
+import jdk.jfr.internal.tool.Main;
+
 import java.util.HashMap;
 
 /**
@@ -7,12 +9,16 @@ import java.util.HashMap;
  * @date 2022/03/03 19:30
  **/
 public class LeetCode_1_两数之和 {
-    public int[] twoSum(int[] nums, int target) {
+    public static void main(String[] args) {
+        int[] q = {1,1,1,1,1,4,1,1,1,1,1,7,1,1,1,1,1};
+        int target = 11;
+        int[] ints = twoSum(q, target);
+    }
+    public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int a = nums[i];
-            if (map.containsKey(target - a)) {
-                return new int[]{map.get(target - a), i};
+            if (map.containsKey(target - nums[i]) && i != map.get(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
             }
             map.put(nums[i], i);
         }
---
+++ src/main/java/com/lil/leetcode/LeetCode_2_两数相加.java
@@ -0,0 +1,42 @@
+package com.lil.leetcode;
+
+/**
+ * @author Lil
+ * @date 2022/03/18 02:53
+ **/
+
+
+class ListNode {
+      int val;
+     ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class LeetCode_2_两数相加 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummary = new ListNode(-1);
+        ListNode cur = dummary;
+
+        int t = 0;
+        while (l1 != null || l2 != null) {
+            if (l1 != null) {
+                t += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                t += l2.val;
+                l2 = l2.next;
+            }
+            cur.next = new ListNode(t % 10);
+            t = t / 10;
+            cur = cur.next;
+        }
+        if (t != 0) {
+            cur.next = new ListNode(t);
+        }
+        return dummary.next;
+    }
+
+}
